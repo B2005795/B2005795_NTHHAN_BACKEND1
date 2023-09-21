192ab387ca69563ab1708ac228ca375f80dfefd6
B2005795 <hanb2005795@student.ctu.edu.vn>
2023-09-21 16:12:18 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,15 @@
+ const express = require("express");
+ const contactsRouter = require("./app/routes/contact.route");
+ const cors = require("cors");
+
+ const app = axpress();
+
+ app.use(cors());
+ app.use(express.json());
+ app.use("/api/contacts", contactsRouter);
+
+ app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application."});
+ });
+
+ module.exports = app;
